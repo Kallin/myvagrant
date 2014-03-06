9dafb48272a537bcc9ffc7eff100d68f131bd13a
knagelberg <knagelberg@salesforce.com>
2014-03-05 17:55:42 -0800

    taking out the trash

--- shell/suppress-terminal-errors.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# suppressing ubuntu terminal errors; https://github.com/mitchellh/vagrant/issues/1673  
-sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
-# bash -c 'BASH_ENV=/etc/profile exec bash'
